7bb3c26a45b74a2407574a12fd130094c93f29ba
mohnas <mn9142001@gmail.com>
2023-10-07 19:16:59 +0300

    refactor request, mixins and renamed views.py

--- mixins.py
+++ mixins.py
@@ -1,3 +1,5 @@
+import json
+from exception import Http400
 
 class SendResponseMixin:
     async def get_scope(self):
@@ -8,7 +10,7 @@ class SendResponseMixin:
         }
         
     async def _send(self, *args, **kwargs):
-        return await self.request.send(*args, **kwargs)
+        return await self.request.sender(*args, **kwargs)
         
     async def send(self, *args, **kwargs):
         scope = await self.get_scope()
@@ -16,4 +18,36 @@ class SendResponseMixin:
             scope
         )
         return await self._send(*args, **kwargs)
+    
+
+class RequestBodyDecoder:
+    
+    async def json_decoder(self, data):
+        try:
+            self._body = json.loads(data)
+        except (json.JSONDecodeError, UnicodeDecodeError) as e:
+            error = f"Error parsing JSON: {str(e)}"
+            raise Http400(error)
+    
+    async def get_raw_body(self):
+        request_body = await self.rec()
+        data = request_body.get('body', b'')
+        return data        
+    
+    async def parse_body(self):
+        data = await self.get_raw_body()
+        
+        if self.headers.is_json:
+            return await self.json_decoder(data)
+        
+        raise Http400("API accepts only json")
+            
+    @property
+    async def body(self):
+        if self.method in ['GET', 'DELETE', 'OPTIONS']:  return
+        
+        if not hasattr(self, '_body'):
+            await self.parse_body()
+        
+        return self._body
     
--- request.py
+++ request.py
@@ -1,15 +1,25 @@
 from functools import cached_property
-import json
-from exception import Http400
 from structs import QueryParameter
-from mixins import SendResponseMixin
+from mixins import SendResponseMixin, RequestBodyDecoder
 
 class Headers:
     def __init__(self, headers={}) -> None:
         [setattr(self, key.decode(), value.decode()) for key, value in headers]
-        
 
-class Request(SendResponseMixin):
+    @property
+    def content_type(self):
+        return getattr(self, 'content-type', "")
+
+    @property
+    def is_json(self):
+        return self.content_type.startswith("application/json")
+    
+    @property
+    def is_multipart(self):
+        return self.content_type.startswith('multipart/form-data')
+
+
+class Request(RequestBodyDecoder, SendResponseMixin):
     kwargs : dict
     method : str
     
@@ -18,7 +28,8 @@ class Request(SendResponseMixin):
         self.scope = scope
         self.set_scope()
         self.headers = Headers(headers=scope.get('headers', []))
-        self.send, self.rec = send, rec
+        
+        self.sender, self.rec = send, rec
         
     def set_scope(self):
         for key, value in self.scope.items():
@@ -34,27 +45,9 @@ class Request(SendResponseMixin):
     
     @property
     def content_length(self):
-        return int((self.headers, b'content-length', b'0'))        
-    
-    @property
-    async def body(self):
-        if not hasattr(self, '_body'):
-            await self.parse_body()
-        return self._body
-    
+        return int((self.headers, b'content-length', b'0'))
+
     @property
     def dest(self):
         return self.path
         
-    async def parse_body(self):
-        if self.method == 'GET':  return
-        
-        request_body = await self.rec()
-        data = request_body.get('body', b'')
-        try:
-            self._body = json.loads(data)
-        except json.JSONDecodeError as e:
-            error = f"Error parsing JSON: {str(e)}"
-            Http400.message = error
-            raise Http400
-            
