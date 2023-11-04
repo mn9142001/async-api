0b7ac6a696e0d7662c59ab919316106ab29ab5b7
mohnas <mn9142001@gmail.com>
2023-11-04 13:15:41 +0200

    added more to test example

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 from async_api.app import App
+from async_api.views import View
 
 app = App()
 
 @app.get('')
 async def index_page(request):
     return "Hello world"
+
+
+class TestView(View):
+    
+    async def get(self):
+        return "hello from test page"
+    
+    
+app.register_as_view("test", TestView.as_view())
