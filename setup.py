0a426b8541fb44b9539147564ceb48977f8c41b7
mohnas <mn9142001@gmail.com>
2023-11-04 07:34:20 +0200

    Added dist files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="async-api",
+    version="0.1",
+    description="An async python backend framework to build fast APIs rapidly.",
+    author="Mohamed Naser",
+    author_email="mohnas914@email.com",
+    url="https://github.com/mn9142001/async-api",
+    packages=find_packages(),
+    install_requires=[
+        "requests-toolbelt==1.0.0",
+        "pydantic==2.4.2"
+    ],
+)
